d4a257117d5387b61a450ce8dd21d125de4dc900
shein <msh86.02.18@rambler.ru>
2021-04-19 10:21:36 +0500

    добавил заполнение таблицы тестовыеми данными

---
+++ src/main/resources/db/migration/V4__insert_tUser.sql
@@ -0,0 +1,10 @@
+INSERT INTO tuser (Name,pass,Mail)
+VALUES
+('user1','pass1', 'user1@mail'),
+('user2','pass2', 'user2@mail'),
+('user3','pass3', 'user3@mail'),
+('user4','pass4', 'user4@mail'),
+('user5','pass5', 'user5@mail'),
+('user6','pass6', 'user6@mail'),
+('user7','pass7', 'user7@mail'),
+('user8','pass8', 'user8@mail');
